78e522351df89eca92eb6bb9d192cc1dc5dc981f
XSNomad <xs_nomad@hotmail.com>
2021-05-18 18:59:10 -0400

    Updated to 1.9.2 - Pilot bool "is Juggernaut" is now based on passive aura Name="JUGGERNAUT". Any Trait, Any Ability, Any Skill that gives a passive aura with the Juggernaut name will enable this function.

--- Source/MightyChargingJuggernaut/Extensions/Pilot.cs
+++ Source/MightyChargingJuggernaut/Extensions/Pilot.cs
@@ -6,23 +6,7 @@ namespace MightyChargingJuggernaut.Extensions
     {
         public static bool IsJuggernaut(this Pilot pilot)
         {
-            return pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Id == "AbilityDefGu8") != null;
-
-            /*
-            bool isJuggernaut = false;
-            if (pilot != null && pilot.PassiveAbilities.Count > 0)
-            {
-                for (int i = 0; i < pilot.PassiveAbilities.Count; i++)
-                {
-                    if (pilot.PassiveAbilities[i].Def.Description.Id == "AbilityDefGu8")
-                    {
-                        isJuggernaut = true;
-                        Logger.Debug("[PilotExtensions_IsJuggernaut] " + pilot.Name + " is a Juggernaut");
-                    }
-                }
-            }
-            return isJuggernaut;
-            */
+            return pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null;
         }
     }
 }
