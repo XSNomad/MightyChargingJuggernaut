56537c72b03ee47dd9aecbe50536adccfc189f0f
XSNomad <xs_nomad@hotmail.com>
2021-05-18 21:02:22 -0400

    Updated to incldue Custom Mech Tagging "BR_MQ_Charger"

--- Source/MightyChargingJuggernaut/Extensions/Pilot.cs
+++ Source/MightyChargingJuggernaut/Extensions/Pilot.cs
@@ -6,7 +6,14 @@ namespace MightyChargingJuggernaut.Extensions
     {
         public static bool IsJuggernaut(this Pilot pilot)
         {
-            return pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null;
+            bool isJuggernaut = false;
+            var mechTags = pilot.ParentActor.GetTags();
+            if (mechTags.Contains("BR_MQ_Charger"))
+                isJuggernaut = true;
+            if (pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null)
+                isJuggernaut = true;
+            Logger.Debug("[PilotExtensions_IsJuggernaut] " + pilot.Name + " is a Juggernaut");
+            return isJuggernaut;
         }
     }
 }
