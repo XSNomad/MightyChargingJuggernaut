9c2a272c9676f9bcafb00222d61dfbbe32427538
XSNomad <60951221+XSNomad@users.noreply.github.com>
2021-06-28 14:16:27 -0400

    updating to 4.7.1

--- Source/MightyChargingJuggernaut/Extensions/Pilot.cs
+++ Source/MightyChargingJuggernaut/Extensions/Pilot.cs
@@ -6,14 +6,7 @@ namespace MightyChargingJuggernaut.Extensions
     {
         public static bool IsJuggernaut(this Pilot pilot)
         {
-            bool isJuggernaut = false;
-            var mechTags = pilot.ParentActor.GetTags();
-            if (mechTags.Contains("BR_MQ_Charger"))
-                isJuggernaut = true;
-            if (pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null)
-                isJuggernaut = true;
-            Logger.Debug("[PilotExtensions_IsJuggernaut] " + pilot.Name + " is a Juggernaut");
-            return isJuggernaut;
+            return pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null;
         }
     }
 }
